f1dfc21b582e0560d165f71701c9ba3e55d0ce68
Hideto Shiozawa <salt-run@Hidetos-MacBook-Pro.local>
2018-01-02 14:46:16 +0900

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'fierce-coast-15819.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'infinite-sierra-69953.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
